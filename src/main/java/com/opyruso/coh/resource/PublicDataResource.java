20101f9867b979560bca80dd0c1f6ccae66f7da5
oPyRuSo <opyruso@gmail.com>
2025-07-20 22:01:47 +0200

    Add public data endpoint

---
+++ src/main/java/com/opyruso/coh/resource/PublicDataResource.java
@@ -0,0 +1,33 @@
+package com.opyruso.coh.resource;
+
+import com.opyruso.coh.entity.Character;
+import com.opyruso.coh.entity.DamageBuffType;
+import com.opyruso.coh.entity.DamageType;
+import com.opyruso.coh.entity.Picto;
+import com.opyruso.coh.entity.Weapon;
+import com.opyruso.coh.model.PublicData;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+
+@Path("/public/data")
+@Produces(MediaType.APPLICATION_JSON)
+public class PublicDataResource {
+
+    @GET
+    @Path("{lang}")
+    @Transactional
+    public PublicData getAll(@PathParam("lang") String lang) {
+        PublicData data = new PublicData();
+        data.characters = Character.find("select distinct c from Character c join fetch c.details d where d.lang = ?1", lang).list();
+        data.damageTypes = DamageType.find("select distinct d from DamageType d join fetch d.details dd where dd.lang = ?1", lang).list();
+        data.damageBuffTypes = DamageBuffType.find("select distinct d from DamageBuffType d join fetch d.details dd where dd.lang = ?1", lang).list();
+        data.pictos = Picto.find("select distinct p from Picto p join fetch p.details d where d.lang = ?1", lang).list();
+        data.weapons = Weapon.find("select distinct w from Weapon w join fetch w.details d where d.lang = ?1", lang).list();
+        return data;
+    }
+}

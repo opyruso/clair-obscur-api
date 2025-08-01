55b14e2189dd49c9a9e895658786aca6c22080c3
oPyRuSo <opyruso@gmail.com>
2025-08-01 03:30:08 +0200

    Implement conditional build update

--- src/main/java/com/opyruso/coh/model/dto/BuildPayload.java
+++ src/main/java/com/opyruso/coh/model/dto/BuildPayload.java
@@ -1,6 +1,7 @@
 package com.opyruso.coh.model.dto;
 
 public class BuildPayload {
+    public String id;
     public String title;
     public String description;
     public Integer recommendedLevel;
